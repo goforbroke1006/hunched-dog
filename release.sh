8d84d0c4189ee27afba36c0f28b38ddad6fec9cb
goforbroke <go.for.broke1006@gmail.com>
2021-02-28 15:06:57 +0300

    Initial commit

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f ./release/*
+
+GOOS=linux   GOARCH=amd64 go build -o ./release/hunched-dog__linux_amd64   ./
+GOOS=linux   GOARCH=386   go build -o ./release/hunched-dog__linux_386     ./
+GOOS=darwin  GOARCH=amd64 go build -o ./release/hunched-dog__darwin_amd64  ./
+GOOS=windows GOARCH=amd64 go build -o ./release/hunched-dog__windows_amd64 ./
+GOOS=windows GOARCH=386   go build -o ./release/hunched-dog__windows_386   ./
