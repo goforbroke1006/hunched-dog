7fd73f55321d19a56206069d13a1c9c9b2c40b48
goforbroke <go.for.broke1006@gmail.com>
2021-03-05 14:48:21 +0300

    versions

--- install/windows.bat
+++ install/windows.bat
@@ -8,7 +8,7 @@ if not exist "%ProgramFiles%\hunched-dog\" (
 )
 
 del "%ProgramFiles%\hunched-dog\hunched-dog.exe"
-CALL curl -L https://github.com/goforbroke1006/hunched-dog/releases/download/v0.1.4/hunched-dog__windows_amd64.exe --output "%ProgramFiles%\hunched-dog\hunched-dog.exe"
+CALL curl -L https://github.com/goforbroke1006/hunched-dog/releases/download/v0.1.5/hunched-dog__windows_amd64.exe --output "%ProgramFiles%\hunched-dog\hunched-dog.exe"
 
 
 :: sc.exe create "hunched-dog" binPath= "%ProgramFiles%\hunched-dog\hunched-dog.exe"
